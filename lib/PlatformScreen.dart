0464e94bf92035720fd7a82e414b45191e8c5e6d
Avinash Choudhary <55015213+Avi-nash5@users.noreply.github.com>
2020-07-17 07:25:17 +0530

    Add files via upload

--- lib/PlatformScreen.dart
+++ lib/PlatformScreen.dart
@@ -15,7 +15,7 @@ class PlatformScreen extends StatefulWidget {
 
 class PlatformScreenState extends State<PlatformScreen> {
   List data;
-
+  int n;
   @override
   void initState() {
     super.initState();
@@ -30,68 +30,125 @@ class PlatformScreenState extends State<PlatformScreen> {
     setState(() {
       data = jsonDecode(response.body);
     });
-
+    n = data.length;
+    print(data);
+    convert();
     return "Success";
+  }
+
+  void convert() {
+    print(n);
+    var now = new DateTime.now();
+    now = now.toLocal();
+    for (int i = 0; i < n; i++) {
+      var start = DateTime.parse(data[i]['start_time']);
+      var end = DateTime.parse(data[i]['end_time']);
+      start = start.toLocal();
+      end = end.toLocal();
+      data[i]['start_time'] = start.toString();
+      data[i]['end_time'] = end.toString();
+    }
+    print(data);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).accentColor,
       body: Column(
         children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0.0, 2.0),
-                      blurRadius: 6.0,
+          Expanded(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 25.0,
                     ),
-                  ],
-                ),
-                child: Hero(
-                  tag: widget.platform.imgurl,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomLeft: Radius.circular(10),
-                            ),
-                          ),
-                          child: Image(
-                            image: AssetImage(widget.platform.imgurl),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Container(
-                          height: 150,
-                          width: MediaQuery.of(context).size.width - 150,
-                          child: Center(
-                            child: Text(
-                              widget.platform.name,
-                              style: TextStyle(
-                                fontSize: 28,
-                                color: Colors.black87,
+                    child: Container(
+                      margin: EdgeInsets.all(10.0),
+                      height: 125,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Stack(
+                        children: <Widget>[
+                          Hero(
+                            tag: widget.platform.imgurl,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(30.0),
+                              child: Image(
+                                height: 125,
+                                width: 125,
+                                image: AssetImage(widget.platform.imgurl),
+                                fit: BoxFit.cover,
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(125, 0, 0, 0),
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  widget.platform.name,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                      ),
+                      child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: n,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  15.0, 15.0, 10.0, 10.0),
+                              child: Container(
+                                height: 150,
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).accentColor,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black54,
+                                        offset: Offset(0, 10.0),
+                                        blurRadius: 10.0,
+                                      )
+                                    ]),
+                                child: Column(
+                                  children: <Widget>[
+                                    Center(
+                                      child: Text(
+                                        data[index]['name'],
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
